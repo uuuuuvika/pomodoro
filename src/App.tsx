1b846b839e050653185eec1225f6235477a54c6f
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-04-08 23:16:53 +0200

    add markup, add base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Break from "./components/break";
+import Session from "./components/session";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Break />
+        <Session />
+      </div>
+      <div id="start_stop">start/stop</div>
+      <div id="reset">reset</div>
     </div>
   );
 }
